afa5cc57a75ed42c101edf4f40222a2c58de5004
dev0507 <69716361+dev0507@users.noreply.github.com>
2021-05-06 21:52:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var Images = ["mom2.jpg","mom3.jpg","mom4.jpg","mom1.jpg"] ;
+var I=0 ;
+function Change(){
+    
+    document.getElementById("mom2.0").src=Images[I];
+    I++;
+   
+document.getElementById("audio").play();
+}
